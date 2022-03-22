6025074577de905367e466c23b779cf23bd63e7e
kiamelJ <90395373+kiamelJ@users.noreply.github.com>
2022-03-22 11:05:30 +0100

    day 2.1

--- src/App.js
+++ src/App.js
@@ -25,14 +25,20 @@ function App() {
     },
   ])
 
+  //Delete task
   const deleteTask = (id) => {
-   console.log('delete', id) 
+   setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
 
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
 }
